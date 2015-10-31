8d0fa139b3b3adde75d9837c82aa08949bc436ae
avared <avared@gmail.com>
2015-10-31 01:42:27 +0000

    uploading all content

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,33 @@
+class PostsController < ApplicationController
+    
+    def index
+        @content_first = "This is sample text for my blog in Ruby";
+        @content_second = "This is my secon sample post for my blog in Ruby";
+    end
+    
+    def new
+        
+    end
+    
+    def create
+        
+    end
+    
+    def edit
+        
+    end
+    
+    def update
+        
+    end
+    
+    def show
+        
+    end
+    
+    def destroy
+        
+    end
+    
+    
+end
